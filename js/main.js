f7e84e19c927dd191e1808c6ddce9a0197a242bb
rusakovsb <rusakov@inbox.ru>
2022-06-26 18:04:15 +0300

    added header

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
    
-// Swiper init
-const swiper = new Swiper('.slider', { 
-	slidesPerView: 1, 
+// Swiper
+const topSlider = new Swiper('.top-slider', { 	 
 	loop: true,  
+	speed: 500,
+	effect: "fade",
+	slidesPerView: 1,
+	spaceBetween: 0,
+	autoplay: {
+		delay: 3000,
+	},
 	navigation: {
 		nextEl: '.slider-next',
 		prevEl: '.slider-prev',
 	},
-	pagination: {
-		el: '.slider-pagination',
-		type: 'bullets',
-		clickable: true
+})
+const itemSlider = new Swiper('.item-slider', { 
+	loop: true,
+	speed: 500,
+	navigation: {
+		nextEl: '.slider-next',
+		prevEl: '.slider-prev',
 	},
+	breakpoints: {
+		320: {
+		  slidesPerView: 1,
+		  spaceBetween: 16,
+		},
+		768: {
+		  slidesPerView: 3,
+		  spaceBetween: 16,
+		},
+		1200: {
+		  slidesPerView: 3,
+		  spaceBetween: 32,
+		},
+	  }
 })
